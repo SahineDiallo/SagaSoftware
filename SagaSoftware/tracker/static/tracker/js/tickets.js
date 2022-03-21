fd3f767d7eb60346c034ad65861f64376a895b37
Papis Sahine <psdiallo300@gmail.com>
2022-03-21 13:10:47 -0700

    finish the basic css of the table tickets

--- SagaSoftware/tracker/static/tracker/js/tickets.js
+++ SagaSoftware/tracker/static/tracker/js/tickets.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $("#tkts-bkl_wrapper")
     var table = $("#tkts-bkl").DataTable({
         'columnDefs': [{
                 "targets": 0, // your case first column
@@ -16,7 +17,35 @@ $(document).ready(function() {
 
     })
 
+    $("#toggle_columns").multipleSelect({
+        width: 200,
+        onClick: function(view) {
+            var selectedCols = $("#toggle_columns").multipleSelect('getSelects')
+            hideAllColumns();
+            for (var i = 0; i <= selectedCols.length; i++) {
+                s = selectedCols[i]
+                table.column(s).visible(1);
+            }
+        },
+        onCheckAll: function() {
+            showAllColumns()
+        },
+        onUncheckAll: function() {
+            hideAllColumns();
+        },
+    })
 
+    //////////// functions ///////////
 
+    function hideAllColumns() {
+        for (var i = 0; i <= 14; i++) {
+            columns = table.column(i).visible(0);
+        }
+    }
 
+    function showAllColumns() {
+        for (var i = 0; i <= 14; i++) {
+            columns = table.column(i).visible(1);
+        }
+    }
 });
